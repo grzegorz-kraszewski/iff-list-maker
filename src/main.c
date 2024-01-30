c34467d2dee3358cd2f4ed02712d6655fce6371e
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2024-01-30 15:30:27 +0100

    First working code. Error handling needs to be streamlined.

--- src/main.c
+++ src/main.c
@@ -11,10 +11,13 @@ struct Library *IFFParseBase;
 struct App
 {
 	LONG IFFType;
+	struct IFFHandle *IFFIn;
 	struct IFFHandle *IFFOut;
+	LONG Error;
+	UBYTE *CopyBuf;
 };
 
-const UBYTE* const IFFErrors[11] = {
+STRPTR IFFErrors[11] = {
 	"reached end of file",
 	"reached end of context",
 	"no scope for property",
@@ -35,6 +38,28 @@ const UBYTE* const IFFErrors[11] = {
 #define ARG_PROP     2
 #define ARG_FROM     3
 #define ARG_FILES    4
+
+#define COPYBUFSIZE  8192
+
+/*---------------------------------------------------------------------------*/
+
+STRPTR GetIFFErrorStr(LONG error)
+{
+	error = -error - 1;   /* [-1 .. -11] translated to [0 .. 10] */
+
+	if ((error >= 0) && (error < 11)) return IFFErrors[error];
+	else return "unknown error";
+}
+
+/*---------------------------------------------------------------------------*/
+
+STRPTR GetDOSErrorStr(LONG error)
+{
+	static UBYTE buf[128];
+
+	Fault(error, "", buf, 128);
+	return buf + 2;               /* skips colon and space */
+}
 
 /*---------------------------------------------------------------------------*/
 
@@ -63,28 +88,167 @@ void PrintErrorIFF(STRPTR message, LONG ifferr)
 
 /*---------------------------------------------------------------------------*/
 
+void RTrim(STRPTR s)
+{
+	STRPTR t = s;
+
+	while (*t) t++;
+
+	while (--t >= s)
+	{
+		if (*t == 0x0A) *t = 0x00;
+		else break;
+	}
+}
+
+/*---------------------------------------------------------------------------*/
+
+LONG CopyLoop(struct App *app)
+{
+	LONG r = 0;
+
+	for (;;)
+	{
+		r = ReadChunkBytes(app->IFFIn, app->CopyBuf, COPYBUFSIZE);
+		if (r <= 0) break;
+		r = WriteChunkBytes(app->IFFOut, app->CopyBuf, r);
+		if (r <= 0)	break;
+	}
+
+	if (r < 0)
+	{
+		Printf("\tCopying FORM failed: %s.\n", GetIFFErrorStr(r));
+		return RETURN_ERROR;
+	}
+
+	return RETURN_OK;
+}
+
+/*---------------------------------------------------------------------------*/
+
+LONG CopyFormData(struct App *app)
+{
+	LONG error = 0;
+	LONG result = RETURN_ERROR;
+
+	if (!(error = PushChunk(app->IFFOut, app->IFFType, ID_FORM, IFFSIZE_UNKNOWN)))
+	{
+		result = CopyLoop(app);
+
+		if (error = PopChunk(app->IFFOut))
+		{
+			Printf("\tFailed to pop FORM chunk from output stack: %s.\n", GetIFFErrorStr(error));
+			result = RETURN_ERROR;
+		}
+		else PutStr("\tDone.\n");
+	}
+	else Printf("/tFailed to push FORM chunk to output stack: %s.\n", GetIFFErrorStr(error));
+
+	return result;
+}
+
+/*---------------------------------------------------------------------------*/
+
+LONG PushFormFromFile(struct App *app, STRPTR filename)
+{
+	LONG result = RETURN_ERROR;
+	BPTR file;
+
+	Printf("%s:\n", filename);
+
+	if (app->IFFIn = AllocIFF())
+	{
+		if (file = Open(filename, MODE_OLDFILE))
+		{
+			LONG ifferr;
+
+			app->IFFIn->iff_Stream = file;
+			InitIFFasDOS(app->IFFIn);
+
+			if ((ifferr = OpenIFF(app->IFFIn, IFFF_READ)) == 0)
+			{
+				if ((ifferr = StopChunk(app->IFFIn, app->IFFType, ID_FORM)) == 0)
+				{
+					if ((ifferr = ParseIFF(app->IFFIn, IFFPARSE_SCAN)) == 0)
+					{
+						result = CopyFormData(app);
+					}
+					else Printf("IFF FORM chunk not found: %s.\n", (LONG)GetIFFErrorStr(ifferr));
+				}
+				else Printf("Failed to set FORM chunk handler: %s.\n", (LONG)GetIFFErrorStr(ifferr));
+
+				CloseIFF(app->IFFIn);
+			}
+			else Printf("\tFailed to open input as IFF: %s.\n", (LONG)GetIFFErrorStr(ifferr));
+
+			Close(file);
+		}
+		else Printf("\tFailed to open input file: %s.\n", (LONG)GetDOSErrorStr(IoErr()));
+
+		FreeIFF(app->IFFIn);
+	}
+	else PutStr("\tOut of memory.\n");
+
+	return result;
+}
+
+/*---------------------------------------------------------------------------*/
+
 LONG PushFormsFromFileList(struct App *app, STRPTR filename)
 {
-	PrintError(0, ERROR_NOT_IMPLEMENTED);
+	STRPTR buf;
+	BPTR file;
+	LONG result = RETURN_OK;
+
+	if (buf = AllocMem(1024, MEMF_ANY))
+	{
+		if (file = Open(filename, MODE_OLDFILE))
+		{
+			LONG doserr;
+
+			while ((result == RETURN_OK) && FGets(file, buf, 1024))
+			{
+				RTrim(buf);
+				result = PushFormFromFile(app, buf);
+			}
+
+			if (doserr = IoErr()) app->Error = doserr;
+			PrintError(NULL, app->Error);
+
+			Close(file);
+		}
+		else PrintError(0, IoErr());
+
+		FreeMem(buf, 1024);
+	}
+	else PrintError(0, ERROR_NO_FREE_STORE);
+
 	return RETURN_ERROR;
 }
 /*---------------------------------------------------------------------------*/
 
 LONG PushFormsFromArgumentList(struct App *app, STRPTR *list)
 {
+	STRPTR filename;
+	LONG result = RETURN_OK;
+
+	while ((filename = *list++) && (result == RETURN_OK))
+	{
+		result = PushFormFromFile(app, filename);
+	}
+
+	return result;
 }
-
-
 
 /*---------------------------------------------------------------------------*/
 
 LONG PushForms(struct App *app, LONG *argtab)
 {
 	LONG result;
-	
+
 	if (argtab[ARG_FROM]) result = PushFormsFromFileList(app, (STRPTR)argtab[ARG_FROM]);
 	else result = PushFormsFromArgumentList(app, (STRPTR*)argtab[ARG_FILES]);
-	
+
 	return result; 
 }
 
@@ -158,6 +322,22 @@ LONG OpenOutput(struct App *app, LONG *argtab)
 
 /*---------------------------------------------------------------------------*/
 
+LONG GetCopyBuffer(struct App *app, LONG *argtab)
+{
+	LONG result = RETURN_FAIL;
+
+	if (app->CopyBuf = AllocMem(COPYBUFSIZE, MEMF_ANY))
+	{
+		result = OpenOutput(app, argtab);
+		FreeMem(app->CopyBuf, COPYBUFSIZE);
+	}
+
+	return result;
+}
+
+/*---------------------------------------------------------------------------*/
+
+
 LONG CheckIFFType(LONG *argtab)
 {
 	long result = RETURN_ERROR;
@@ -167,7 +347,7 @@ LONG CheckIFFType(LONG *argtab)
 
 	if (GoodType(app.IFFType))
 	{
-		result = OpenOutput(&app, argtab);
+		result = GetCopyBuffer(&app, argtab);
 	}
 	else
 	{
@@ -185,7 +365,7 @@ LONG ParseArguments(void)
 {
 	LONG result = RETURN_ERROR;
 	struct RDArgs *args;
-	LONG argtab[5];
+	LONG argtab[5] = { 0, 0, 0, 0, 0 };
 
 	if (args = ReadArgs("TO/K/A,TYPE/K/A,PROP/K,FROM/K,FILES/M", argtab, NULL))
 	{
